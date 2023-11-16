c764b769fe0f2c8c44cd15fcc0b0c628b10134a0
Sergejs Umnovs <sergio30032001@gmail.com>
2023-11-16 14:01:52 +0200

    To be finished

--- src/main.m
+++ src/main.m
@@ -1,17 +1,19 @@
 f = 100;
+T = 1/f;
 
-N = 1024;
-tau = T/N;
+t_b = 16*T;
+N = 8192;
+tau = t_b/N;
 Fs = 1/tau;
 
+t = 0:tau:t_b-tau;
+S_m_sin = 1;
+s_sin_gen = @(t) S_m_sin*(sin(2*pi*f*t));
+s_sin = s_sin_gen(t);
 
-%% Test signal
-
-for i = 0:N-1
-  S_m_sin = 1;
-  t = 0:tau:T+i*tau;
-  s_sin_gen = @(t) S_m_sin*(sin(2*pi*f*t) + sin(0*2*pi*f*t));
-  s_sin = s_sin_gen(t);
+for i = 0:10:N-1
+  
+  win = [hamming(i)' zeros(1, N-i)];
 
   %% FFT
   f_step = Fs/length(t);
@@ -19,24 +21,30 @@ for i = 0:N-1
   S_sin_abs = abs(fft(s_sin))/length(f_fft);
   S_sin_abs = fftshift(S_sin_abs);
 
+  s_sin_win = s_sin.*win;
+  S_sin_win_abs = abs(fft(s_sin_win))/length(f_fft);
+  S_sin_win_abs = fftshift(S_sin_win_abs);
 
   %% Plots
 
   figure(1)
-  subplot(2,1,1);
+  subplot(3,1,1);
   plot(t,s_sin);
 
-  subplot(2,1,2);
-  plot(f_fft, S_sin_abs);
-  xlim([-Fs/2 Fs/2-f_step]);
-  ylim([0 1]);
-      pause(0.1);
+  subplot(3,1,2);
+  plot(f_fft, S_sin_win_abs);
+  xlim([-3*f/2 3*f/2-f_step]);
+  %ylim([0 1]);
+
+  subplot(3,1,3);
+  plot(t, s_sin_win);
 end
 
 
 %% tasks
 %
-% 1. Create test signals : cosines, AM, FM
+% 1. Create test signals : cosines, AM, FM, BFSK, BASK
 % FFT needs many samples
 % Multiply long signal of fixed length with a window of a variable length
+
 % 
